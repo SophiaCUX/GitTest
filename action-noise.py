705d87431e8e88280a1ce78b4576fa4aa7eefe6d
SophiaCUX <48795719+SophiaCUX@users.noreply.github.com>
2019-03-21 14:41:39 +0100

    Add files via upload

---
+++ action-noise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*-: coding utf-8 -*-
+
+from hermes_python.hermes import Hermes
+import json
+
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+
+
+INTENT_LOUD = "Loud"
+
+SessionsStates = {}
+
+def user_loud(hermes, intent_message):
+    print("User is asking for noise")
+
+    session_id = intent_message.session_id
+
+    hermes.publish_end_session(session_id, None)
+
+
+with Hermes(MQTT_ADDR) as h:
+
+    h.subscribe_intent(INTENT_LOUD, user_loud) \
+        .start()
